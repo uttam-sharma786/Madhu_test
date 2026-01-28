0594e7f4c046a662c8546b06c34c3d1fb1ca2b42
uttam-sharma786 <uttamsharma.8560@gamil.com>
2026-01-28 21:20:23 +0530

    promise

--- 02_Basics/promise.js
+++ 02_Basics/promise.js
@@ -1,27 +1,29 @@
-const fetechPromise = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
-console.log(fetechPromise);
-// console.log(fetechPromise.status);
+// const fetechPromise = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+// console.log(fetechPromise);
+// // console.log(fetechPromise.status);
 
-fetechPromise.then((response) => {
-    if(!response.ok){
-        throw new error (`HTTP error: ${response.status}`)
-    }
-    return response.json();
-})
+const { response } = require("express");
 
-.then((data)=>{
-    console.log(data[200].name);
-    
-})
-.catch((error)=>{
-    console.log(`could not get any product: ${error}`);
-    
-})
+// fetechPromise.then((response) => {
+//     if(!response.ok){
+//         throw new error (`HTTP error: ${response.status}`)
+//     }
+//     return response.json();
+// })
 
-.finally((show)=>{
-    console.log(`show the any result ${show}`);
+// .then((data)=>{
+//     console.log(data[200].name);
     
-})
+// })
+// .catch((error)=>{
+//     console.log(`could not get any product: ${error}`);
+    
+// })
+
+// .finally((show)=>{
+//     console.log(`show the any result ${show}`);
+    
+// })
   
 // jsonPromise.then((data) => {
 //     console.log(`${data[2].price}`);
@@ -31,6 +33,60 @@ fetechPromise.then((response) => {
 
 
 
+// const apiFetch = fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random");
 
+// apiFetch
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     console.log(data.data.author);
+//   })
+//   .catch((error) => {
+//     console.log(`Error: ${error}`);
+//   });
+
+// const jokesData = fetch("https://api.freeapi.app/api/v1/public/randomjokes");
+// jokesData.then((response)=>{
+//     const jokesresponse = response.json()
+//     console.log(jokesresponse);
+//     console.log(`Received response : ${response.headers}`);
+//     jokesresponse.then((data)=>{
+//         console.log(data);
+        
+//     })
     
+    
+// })
 
+// const bookURL = fetch("https://api.freeapi.app/api/v1/public/books")
+
+// bookURL.then((response) => {
+//     const bookResponse = response.json()
+//     console.log(`check reponse ${response.status}`);
+//     console.log(bookResponse);
+
+//     bookResponse.then((data) => {
+//         console.log(data);
+        
+//     })
+    
+    
+    
+// })
+
+const quoteUrl = fetch("https://api.freeapi.app/api/v1/public/quotes");
+quoteUrl.then((response) => {
+    const quoteResponse = response.json()
+    console.log(`quoteresponse ${response.status}`);
+    console.log(quoteResponse);
+
+
+    quoteResponse.then((data) =>{
+        console.log(data.data.data.data[author]);
+        console.log(data);
+        
+        
+        
+    })
+    
+})
